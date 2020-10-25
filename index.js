527821e0e29d59e1ad1cb36a4aa498e295c3676f
shateva <72813500+shateva@users.noreply.github.com>
2020-10-25 18:57:07 -0400

    Update index.js
    Array holding the phrases that display racial bias

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 
+//This array holds the bias phrases and words
+//note: take in to account capitalization, whether the terms end in 's', and whether they're a part of a quote
+let phrases = ['Illegal Aliens', 'Illegal Alien', 'Oriental', 'Orientals', 'Illegals', 'The blacks', 'The asians', 'The whites', 'The latinos']
+
+console.log(phrases.length)
