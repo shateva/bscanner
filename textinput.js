f05078213f8534a08cadea6761da8222bfa37d38
almanzaj220 <72989227+almanzaj220@users.noreply.github.com>
2020-11-02 00:08:49 -0500

    added an event listener to save the text input

--- textinput.js
+++ textinput.js
@@ -1,6 +1,17 @@
-function saveText(){
-    var userInput = document.getElementById("txt").value;    
-	console.log(userInput);
+// global variables that allow us to access contents of
+// the textbox and the scan button
+const textbox = document.getElementById("txt");
+const scanbtn = document.getElementById("scanning");
+
+// calls saveText function when you click the scan button
+scanbtn.addEventListener("click", scanText);
+
+// function that will scan the text
+function scanText(){
+    var userInput = textbox.value;    
+    console.log(userInput);
+    
+    
 }
 
 saveText();
