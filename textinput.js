092f1ab3fd7cf1857a5765cc66601c439b98970b
almanzaj220 <72989227+almanzaj220@users.noreply.github.com>
2020-11-15 10:16:10 -0500

    added some scrolling

--- textinput.js
+++ textinput.js
@@ -66,8 +66,38 @@ var i;
 // array for the phrases found in userInput
 var phrasesFound = new Array();
 
+// scrolls the page down, idk how it works
+function scroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+    
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+
 // function that will scan the text
 function scanText() {
+    // scrolling
+    scroll("#scanning", 500);
+
     // removes any alerts that already exist so that
     // alerts aren't repeated
     while (alert.hasChildNodes()) {   
