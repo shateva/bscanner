e670c304fff553c96391d2d8dc2f8bf0e5773bbb
shateva <72813500+shateva@users.noreply.github.com>
2020-11-21 12:30:12 -0500

    removed scroll function, not needed

--- textinput.js
+++ textinput.js
@@ -64,6 +64,7 @@ var userInput;
 // temp is the same as userInput, but not lowercase
 var temp;
 var i;
+
 // array for the phrases found in userInput
 var phrasesFound = new Array();
 
@@ -77,31 +78,6 @@ var id_6;
 
 
 
-// scrolls the page down, idk how it works
-function scroll(target, duration) {
-    var target = document.querySelector(target);
-    var targetPosition = target.getBoundingClientRect().top;
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
-    
-    function animation(currentTime) {
-        if (startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
-        var run = ease(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0, run);
-        if (timeElapsed < duration) requestAnimationFrame(animation);
-    }
-
-    function ease(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
-    }
-
-    requestAnimationFrame(animation);
-}
 
 
 // function that will scan the text
